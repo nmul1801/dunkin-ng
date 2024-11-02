8f04cd2998f3f3c3d8d15e4b311515f174b4dea9
nmul1801 <nicholas.m.mulligan@gmail.com>
2024-11-02 17:33:16 -0400

    no jekyll

--- src/app/dunkin.service.ts
+++ src/app/dunkin.service.ts
@@ -7,11 +7,13 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class DunkinService {
-  private apiUrl = 'http://localhost:3000/api';
+  // private apiUrl = 'http://localhost:3000/api';
+  private apiUrl = 'https://dunkin-scout-api-3f40821b6dbb.herokuapp.com/api';
 
   constructor(private http: HttpClient) {}
 
   getMenuItems(lat: number, long: number, dist: number): Observable<any> {
+    console.log(`${this.apiUrl}/menu-items?lat=${lat}&long=${long}&dist=${dist}`);
     return this.http.get(`${this.apiUrl}/menu-items?lat=${lat}&long=${long}&dist=${dist}`);
   }
 
